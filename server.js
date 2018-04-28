92c733c37194f071888d420c9e39e996f5b07724
aimosjo <35940931+aimosjo@users.noreply.github.com>
2018-04-28 11:42:02 -0600

    History Update
    Upon new connection, chat history will now be displayed for new comers

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var app = express();
 var http = require('http');
 var server = http.Server(app);
 
+var chatLog = [];
+
 app.use(express.static('client'));
 
 var io = require('socket.io')(server);
@@ -14,6 +16,29 @@ io.on('connection', function (socket) {
   });
 });
 
+io.on('connection', function (socket) {
+	socket.on('message', function (msg) {
+		chatLog.push(msg);
+	});
+});
+
+//this will specify that any time a new user logs in,
+//the system will print out the messages stored in chatLog
+io.on('connection', function (socket) {
+	for (var i = 0; i < chatLog.length; i++) {
+		socket.emit('message', chatLog[i]);
+	}
+});
+
+
+
 server.listen(8080, function() {
   console.log('Chat server running');
+});
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
